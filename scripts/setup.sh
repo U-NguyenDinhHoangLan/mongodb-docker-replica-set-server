a7988a2b2327d52acd5f15c5d6e48ea533c90870
U-NguyenDinhHoangLan <136219100+U-NguyenDinhHoangLan@users.noreply.github.com>
2023-06-11 12:20:51 +0700

    Update console message, remove dead code

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-#MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-#MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
-
 MONGODB1=mongo1
 MONGODB2=mongo2
 MONGODB3=mongo3
@@ -12,16 +8,11 @@ PORT1=30001
 PORT2=30002
 PORT3=30003
 
-echo "**********************************************" ${MONGODB1}
-echo "Waiting for startup.."
+echo "Waiting for ${MONGODB1}:${PORT1} to startup.."
 until curl http://${MONGODB1}:${PORT1}/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
-
-# echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
-# echo "Started.."
-
 
 echo SETUP.sh time now: `date +"%T" `
 mongo --host ${MONGODB1} --port ${PORT1} <<EOF
